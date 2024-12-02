bc4b5abc5a3611eafc8f64e8e62960ea02c83478
JamesVeth <javehosting01@gmail.com>
2024-12-02 22:07:09 +0000

    Changes

--- TestingStuff_01.js
+++ TestingStuff_01.js
@@ -1,17 +1,26 @@
 const gameArea = document.getElementById('gameArea');
 const gameCanvas = document.getElementById('gameCanvas');
 const ctx = gameCanvas.getContext('2d');
+const labelScore = document.getElementById('labelScore');
+const buttonReset = document.getElementById('buttonReset');
 
 
 
 let gameRunning = false;
 let unitSize = 25;
 let xVelocity = unitSize;
+let yVelocity = 0;
 let foodX;
 let foodY;
 let score = 0;
 let gameWidth = gameCanvas.getWidth;
 let gameHeight = gameCanvas.getHeight;
+
+// Add Event Listeners (which esesentially run in a type of thread, continuously)
+buttonReset.addEventListener("click", gameStart);
+gameCanvas.addEventListener("keydown", changeDirection);
+
+gameStart();
 
 let snake = [
     {x:unitSize*4,y:0},
@@ -37,18 +46,32 @@ function randomFood(min, max) {
     // 7 * 25 = 175
     // So we draw at 175, which is a divisible of 25, so the piece doesn't overlap
 
-    let randNum = Math.round((Math.random() * (max-min) + min) / unitSize) * unitSize;
+    let randNum = Math.round((Math.random() * (max - unitSize) / unitSize) * unitSize;
     return randNum;
 
 }
 
-function clearBoard(){
-
+function gameStart(){
+    
+    gameRunning = true;
+    labelScore.textContent = score;
+    createFood();
+    drawFood();
+    drawSnake();
 
 }
 
-function resetGame(){
 
+function resetGame(){
+    score = 0;
+    snake = {x:unitSize*4,y:0},
+    {x:unitSize*3,y:0},
+    {x:unitSize*2,y:0},
+    {x:unitSize*1,y:0},
+    {x:0,y:0};
+    xVelocity = unitSize;
+    yVelocity = 0;
+    // 
 }
 
 function checkGameOver() {
