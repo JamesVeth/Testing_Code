a1899e82c8a9f0ca8fd02dc3cc21d335d9dc779b
JamesVeth <javehosting01@gmail.com>
2024-12-09 21:52:21 +0000

    Changes

--- TestingStuff_01.js
+++ TestingStuff_01.js
@@ -33,7 +33,7 @@ window.addEventListener("keydown", changeDirection);
 buttonReset.addEventListener("click", resetGame);
 
 
-displayGameOver();
+
 
 function gameStart(){
     
@@ -85,7 +85,7 @@ function randomFood(min, max) {
     // 7 * 25 = 175
     // So we draw at 175, which is a divisible of 25, so the piece doesn't overlap
 
-    let randNum = Math.floor((Math.random() * (max - min) / unitSize)) * unitSize + min;
+    let randNum = Math.floor(Math.random() * ((max - min) / unitSize)) * unitSize + min;
     return randNum;
 
 }
@@ -97,11 +97,13 @@ function resetGame(){
     xVelocity = unitSize;
     yVelocity = 0;
 
-    snake = [{x:unitSize*4,y:0},
-    {x:unitSize*3,y:0},
-    {x:unitSize*2,y:0},
-    {x:unitSize*1,y:0},
-    {x:0,y:0}];
+    snake = [
+        {x:unitSize*4,y:0},
+        {x:unitSize*3,y:0},
+        {x:unitSize*2,y:0},
+        {x:unitSize,y:0},
+        {x:0,y:0}
+    ];
     
     gameStart();
 
@@ -153,7 +155,7 @@ function drawSnake(){
     snake.forEach(snakeSegment=>{
         ctx.fillRect(snakeSegment.x, snakeSegment.y, unitSize, unitSize);
         ctx.strokeRect(snakeSegment.x, snakeSegment.y, unitSize, unitSize);
-    });
+    })
 
 
 }
@@ -172,18 +174,22 @@ function changeDirection(event) {
 
         case (keyPressed == "ArrowUp" && !goingDown):
             yVelocity = -unitSize;
+            xVelocity = 0;
             break;
         
         case (keyPressed == "ArrowDown" && !goingUp):
             yVelocity = unitSize;
+            xVelocity = 0;
             break;
         
         case (keyPressed == "ArrowLeft" && !goingRight):
             xVelocity = -unitSize;
+            yVelocity = 0;
             break;
         
         case (keyPressed == "ArrowRight" && !goingLeft):
             xVelocity = unitSize;
+            yVelocity = 0;
             break;
         
     }   
@@ -201,8 +207,8 @@ function clearBoard(){
 
 function moveSnake(){
 
-    const head = {x:snake[0].x, 
-                    y:snake[0].y}
+    const head = {x:snake[0].x + xVelocity, 
+                    y:snake[0].y + yVelocity}
     
     /*
 
