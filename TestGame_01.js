cdd4898c47f30754c6d1cad700e744f285cbfb1b
James V <your.email@example.coDDm>
2024-12-10 20:07:16 +0000

    Changes

---
+++ TestGame_01.js
@@ -0,0 +1,31 @@
+const canvasGame = document.querySelector("#canvasGame");
+const divScore = document.querySelector("#divScore");
+const buttonGame = document.querySelector("#buttonStart");
+
+function resetGame(){
+
+    
+}
+
+function gameStart(){
+
+}
+
+function nextTick(){
+
+
+}
+
+function drawSnake(){
+
+}
+
+function drawFood(){
+
+}
+
+function createFood(){
+
+
+}
+
